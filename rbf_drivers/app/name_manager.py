b9bf73ed8fa55ddad69cdaffd8decc2fb8312830
James Snowden <jamesvsnowden@me.com>
2022-05-13 18:23:35 +0200

    debug logging

--- rbf_drivers/app/name_manager.py
+++ rbf_drivers/app/name_manager.py
@@ -1,5 +1,6 @@
 
-from typing import TYPE_CHECKING, Optional, Sequence
+from typing import TYPE_CHECKING, Iterable, List, Optional, Sequence
+from logging import getLogger
 from .events import event_handler
 from .utils import owner_resolve
 from ..api.input_target import InputTargetBoneTargetUpdateEvent, InputTargetObjectUpdateEvent
@@ -13,11 +14,14 @@ from ..api.outputs import OutputNewEvent
 from ..api.driver import DriverNameUpdateEvent
 from ..api.drivers import DriverNewEvent
 if TYPE_CHECKING:
+    from bpy.types import PropertyGroup
     from ..api.input_target import RBFDriverInputTarget
     from ..api.input_variables import RBFDriverInputVariables
     from ..api.input import RBFDriverInput
     from ..api.output import RBFDriverOutput
     from ..api.driver import RBFDriver
+
+log = getLogger(__name__)
 
 DEFAULT_INPUT_NAMES = {
     'LOCATION'     : "Location",
@@ -38,7 +42,14 @@ DEFAULT_OUTPUT_NAMES = {
     }
 
 
-def uniqname(value: str, names: Sequence[str], separator: Optional[str]=".", zfill: Optional[int]=3) -> str:
+def siblings(item: 'PropertyGroup', collection: Iterable['PropertyGroup']) -> List[str]:
+    return [member.name for member in collection if member != item]
+
+
+def uniqname(value: str,
+             names: Sequence[str],
+             separator: Optional[str]=".",
+             zfill: Optional[int]=3) -> str:
     i = 0
     k = value
     while k in names:
@@ -81,7 +92,7 @@ def inputname(input: 'RBFDriverInput') -> str:
 
     else:
         driver: 'RBFDriver' = owner_resolve(input, ".inputs")
-        result = uniqname("Input", [x.name for x in driver.inputs if x != input])
+        result = uniqname("Input", siblings(input, driver.inputs))
 
     return result
 
@@ -118,21 +129,30 @@ def drivername(driver: 'RBFDriver') -> str:
     if driver.type == 'SHAPE_KEYS':
         return f'Shape Keys ({driver.id_data.name})'
     else:
-        return uniqname("Driver", [x.name for x in driver.id_data.rbf_drivers if x != driver])
+        return uniqname("Driver", siblings(driver, driver.id_data.rbf_drivers))
+
+
 
 
 @event_handler(DriverNewEvent)
 def on_driver_new(event: DriverNewEvent) -> None:
     driver = event.driver
     if not driver.name:
-        driver["name"] = drivername(event.driver)
+        value = drivername(driver)
+
+        log.debug((f'Assigning auto-generated name "{value}" to new '
+                   f'{driver} because no name was supplied by the user.'))
+        driver["name"] = value
 
 
 @event_handler(DriverNameUpdateEvent)
 def on_driver_name_update(event: DriverNameUpdateEvent) -> None:
+    driver = event.driver
     if not event.value:
-        driver = event.driver
-        driver["name"] = drivername(driver)
+        value = drivername(driver)
+        log.debug((f'Assigning auto-generated name "{value}" to '
+                    f'{driver} because user set name to empty string.'))
+        driver["name"] = value
 
 
 @event_handler(InputNewEvent)
@@ -141,10 +161,13 @@ def on_input_new(event: InputNewEvent) -> None:
     type = input.type
 
     if type == 'USER_DEF':
-        driver: 'RBFDriver' = owner_resolve(event.input, ".inputs")
-        event.input["name"] = uniqname("Input", [x.name for x in driver.inputs if x != input])
+        driver: 'RBFDriver' = owner_resolve(input, ".inputs")
+        value = uniqname("Input", siblings(input, driver.inputs))
     else:
-        event.input["name"] = DEFAULT_INPUT_NAMES[event.input.type]
+        value = DEFAULT_INPUT_NAMES[input.type]
+
+    log.debug(f'Assigning auto-generated name "{value}" to new {input}')
+    input["name"] = value
 
 
 @event_handler(InputNameUpdateEvent)
@@ -153,39 +176,69 @@ def on_input_name_update(event: InputNameUpdateEvent) -> None:
     value = event.value
     if value:
         driver: 'RBFDriver' = owner_resolve(input, ".inputs")
+
+        log.debug((f'Setting name_is_user_defined to True for {input}'
+                   f'because the user set name to non-empty string.'))
         input["name_is_user_defined"] = True
-        input["name"] = uniqname(value, [x.name for x in driver.inputs if x != input])
+
+        value = uniqname(value, siblings(input, driver.inputs))
+        if value != event.value:
+            log.debug((f'Assigning name "{value}" to {input} because '
+                       f'user-defined name "{event.value}" was not unique'))
+            input["name"] = value
     else:
+        value = inputname(input)
+
+        log.debug((f'Setting name_is_user_defined to False for {input}'
+                   f'because the user set name to empty string.'))
         input["name_is_user_defined"] = False
-        input["name"] = inputname(input)
+
+        log.debug((f'Assigning auto-generated name "{value}" to '
+                   f'{input} because the user set name to empty string.'))
+        input["name"] = value
 
 
 @event_handler(InputObjectUpdateEvent)
 def on_input_object_update(event: InputObjectUpdateEvent) -> None:
     input = event.input
     if not input.name_is_user_defined and input.type != 'USER_DEF':
-        input["name"] = inputname(input)
+        value = inputname(input)
+
+        log.debug((f'Assigning auto-generated name "{value}" '
+                   f'to {input} due to input object property update'))
+        input["name"] = value
 
 
 @event_handler(InputBoneTargetUpdateEvent)
 def on_input_bone_target_update(event: InputBoneTargetUpdateEvent) -> None:
     input = event.input
     if not input.name_is_user_defined and input.type != 'USER_DEF':
-        input["name"] = inputname(input)
+        value = inputname(input)
+
+        log.debug((f'Assigning auto-generated name "{value}" '
+                   f'to {input} due to input bone_target property update'))
+        input["name"] = value
 
 
 @event_handler(InputTargetObjectUpdateEvent)
 def on_input_target_object_update(event: InputTargetObjectUpdateEvent) -> None:
     input: 'RBFDriverInput' = owner_resolve(event.target, ".variables")
     if input.type.endswith('DIFF') and not input.name_is_user_defined:
-        input["name"] = inputname(input)
+        value = inputname(input)
+
+        log.debug((f'Assigning auto-generated name "{value}" '
+                   f'to {input} due to target object property update'))
+        input["name"] = value
 
 
 @event_handler(InputTargetBoneTargetUpdateEvent)
 def on_input_target_bone_target_update(event: InputTargetBoneTargetUpdateEvent) -> None:
     input: 'RBFDriverInput' = owner_resolve(event.target, ".variables")
     if input.type.endswith('DIFF') and not input.name_is_user_defined:
-        input["name"] = inputname(input)
+        value = inputname(input)
+        log.debug((f'Assigning auto-generated name "{value}" '
+                   f'to {input} due to target bone_target property update'))
+        input["name"] = value
 
 
 @event_handler(InputVariableNewEvent)
@@ -193,38 +246,65 @@ def on_input_variable_new(event: InputVariableNewEvent) -> None:
     variable = event.variable
     if not variable.name:
         variables: 'RBFDriverInputVariables' = owner_resolve(variable, ".")
-        variable["name"] = uniqname("var", [x.name for x in variables if x != variable], separator="_", zfill=2)
+
+        value = "var"
+        value = uniqname(value, siblings(variable, variables), separator="_", zfill=2)
+
+        log.debug(f'Assigning auto-generated name "{value}" to new {variable}')
+        variable["name"] = value
 
 
 @event_handler(PoseNameUpdateEvent)
 def on_pose_name_update(event: PoseNameUpdateEvent) -> None:
     pose = event.pose
-    driver: 'RBFDriver' = owner_resolve(pose, ".poses")
-    pose["name"] = uniqname(event.value, [x.name for x in driver.poses if x != pose])
+    value = uniqname(event.value, siblings(pose, owner_resolve(pose, ".poses").poses))
+
+    if value != event.value:
+        log.debug((f'Assigning name "{value}" to {pose} because '
+                   f'user-defined name "{event.value}" was not unique'))
+
+        pose["name"] = value
 
 
 @event_handler(OutputNewEvent)
 def on_output_new(event: OutputNewEvent) -> None:
-    if event.output.type == 'SHAPE_KEY':
-        driver: 'RBFDriver' = owner_resolve(event.output, ".outputs")
-        if driver.type == 'SHAPE_KEY':
-            event.output["name_is_user_defined"] = True
-            return
-    event.output["name"] = DEFAULT_OUTPUT_NAMES[event.output.type]
+    output = event.output
+    driver: 'RBFDriver' = owner_resolve(output, ".outputs")
+
+    if output.type == 'SHAPE_KEY' and driver.type == 'SHAPE_KEY':
+        log.debug((f'Setting name_is_user_defined to True on '
+                   f'{output} because driver.type == "SHAPE_KEY"'))
+        output["name_is_user_defined"] = True
+        return
+
+    value = DEFAULT_OUTPUT_NAMES[output.type]
+    value = uniqname(value, siblings(output, driver.outputs))
+
+    log.debug((f'Assigning name "{value}" to new output {output}'))
+    output["name"] = value
 
 
 @event_handler(OutputObjectChangeEvent)
 def on_output_object_update(event: OutputObjectChangeEvent) -> None:
     output = event.output
+
     if not output.name_is_user_defined and output.type != 'SINGLE_PROP':
-        output["name"] = outputname(output)
+        value = outputname(output)
+
+        log.debug((f'Assigning auto-generated name "{value}" '
+                   f'to {output} due to object property update'))
+        output["name"] = value
 
 
 @event_handler(OutputBoneTargetChangeEvent)
 def on_output_bone_target_update(event: OutputBoneTargetChangeEvent) -> None:
     output = event.output
     if not output.name_is_user_defined and output.type != 'SINGLE_PROP':
-        output["name"] = outputname(output)
+        value = outputname(output)
+
+        log.debug((f'Assigning auto-generated name "{value}" '
+                   f'to {output} due to bone_target property update'))
+        output["name"] = value
 
 
 @event_handler(OutputNameUpdateEvent)
@@ -233,14 +313,37 @@ def on_output_name_update(event: OutputNameUpdateEvent) -> None:
     driver: 'RBFDriver' = owner_resolve(output, ".outputs")
 
     if event.value:
+        log.debug((f'Setting name_is_user_defined to True for {output}'
+                   f'because the user set name to non-empty string.'))
         output["name_is_user_defined"] = True
-        output["name"] = uniqname(event.value, [x.name for x in driver.outputs if x != output])
+
+        cache = event.value
+        value = uniqname(cache, siblings(output, driver.outputs))
+
+        if value != cache:
+            log.debug((f'Assigning name "{value}" to {output} because '
+                       f'user-defined name "{cache}" was not unique'))
+            output["name"] = value
     else:
+        value = outputname(output)
+
+        log.debug((f'Setting name_is_user_defined to False for {output}'
+                   f'because the user set name to empty string.'))
         output["name_is_user_defined"] = False
-        output["name"] = outputname(output)
+
+        log.debug((f'Assigning auto-generated name "{value}" to '
+                   f'{output} because the user set name to empty string.'))
+        output["name"] = value
+
 
 @event_handler(OutputChannelNameChangeEvent)
 def on_output_channel_name_change(event: OutputChannelNameChangeEvent) -> None:
     output: 'RBFDriverOutput' = owner_resolve(event.channel, ".channels")
+
     if output.type == 'SHAPE_KEY' and not output.name_is_user_defined:
-        output["name"] = outputname(output)
+        value = outputname(output)
+
+        log.debug((f'Assigning auto-generated name "{value}" '
+                   f'to {output} due to channel name property '
+                   f'update because output.type is "SHAPE_KEY"'))
+        output["name"] = value
